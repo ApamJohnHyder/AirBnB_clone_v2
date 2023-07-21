e4242517db04ec5d0440575b27815ce0f221ac98
Samuel Fosu <sfosu003@gmail.com>
2023-07-20 20:03:07 -0700

    Add Project Airbnb files

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,114 +1,45 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""Starts a Flask web application.
+"""starts a Flask web application"""
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
-    /python/(<text>): Displays 'Python' followed by the value of <text>.
-    /number/<n>: Displays 'n is a number' only if <n> is an integer.
-    /number_template/<n>: Displays an HTML page only if <n> is an integer.
-"""
-from flask import Flask
-from flask import render_template
-=======
-""" runs an app with Flask framework """
 from flask import Flask, render_template
-
->>>>>>> fa35fa4d506a850808a3e11a3c43c384b95f2deb
-
 app = Flask(__name__)
 
 
-<<<<<<< HEAD
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Displays 'HBNB'"""
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>
-
-    Replaces any underscores in <text> with slashes.
-    """
-    text = text.replace("_", " ")
-    return "C {}".format(text)
-
-
-@app.route("/python", strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
-def python(text="is cool"):
-    """Displays 'Python' followed by the value of <text>
-
-    Replaces any underscores in <text> with slashes.
-    """
-    text = text.replace("_", " ")
-    return "Python {}".format(text)
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def number(n):
-    """Displays 'n is a number' only if <n> is an integer."""
-    return "{} is a number".format(n)
-
-
-@app.route("/number_template/<int:n>", strict_slashes=False)
-def number_template(n):
-    """Displays an HTML page only if <n> is an integer."""
-    return render_template("5-number.html", n=n)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-=======
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Function called with / route """
+def index():
+    """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Function called with /hbnb route """
+    """returns HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """ Function called with /c/<text> route """
-    return 'C %s' % text.replace('_', ' ')
+def cisfun(text):
+    """display “C ” and value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python/', strict_slashes=False)
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):
-    """ Function called with /python/<text> route """
-    if text is not 'is cool':
-        text = text.replace('_', ' ')
-    return 'Python %s' % text
+def pythoniscool(text='is cool'):
+    """display Python"""
+    return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """ Function called with /number/<n> route """
-    return "%d is a number" % n
+def imanumber(n):
+    """display “n is a number” only if n is an integer"""
+    return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    """ Function called with /number_template/<n> route """
-    return render_template('5-number.html', number=n)
+def numbersandtemplates(n):
+    """display HTML page if n is an integer"""
+    return render_template('5-number.html', n=n)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
->>>>>>> fa35fa4d506a850808a3e11a3c43c384b95f2deb
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,61 +1,23 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""Starts a Flask web application.
+"""Script starts a Flask web application"""
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
-"""
-from models import storage
-from flask import Flask
-from flask import render_template
-
-app = Flask(__name__)
-
-
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
-
-
-@app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
-    storage.close()
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-=======
-""" runs an app with Flask framework """
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-
-
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def teardown_session(exception):
-    """ Teardown """
-    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
-def display_html():
-    """ Function called with /states_list route """
-    states = storage.all(State)
-    dict_to_html = {value.id: value.name for value in states.values()}
-    return render_template('7-states_list.html',
-                           Table="States",
-                           items=dict_to_html)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
->>>>>>> fa35fa4d506a850808a3e11a3c43c384b95f2deb
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
